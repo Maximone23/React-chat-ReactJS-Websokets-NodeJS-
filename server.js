60e05598143d9c310ea5cb2ea1638e6d71c83722
Maximone23 <maxim.one23@gmail.com>
2021-04-23 16:34:14 +0300

    Add socket.io

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
+//Connecting the ExpressJS Framework
 const express = require('express');
+//Connecting Socket.io
+const ws = require('socket.io');
 
 const app = express();
 
-app.get('/users', function() {
+const rooms = new Map();
 
+app.get('/rooms', (req, res) => {
+    rooms.set()
+    res.json(rooms)
 });
 
-app.listen(5555);
+app.listen(5555, (err) => {
+    if (err) {
+        throw Error(err)
+    }
+    console.log("Start server!");
+});
